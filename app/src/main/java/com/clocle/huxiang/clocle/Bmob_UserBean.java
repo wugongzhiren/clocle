1bb199b028590b7a771cf3a96a9bfcad6f690b33
huxiang <1406999464@qq.com>
2016-09-19 21:44:10 +0800

    huxiang2016907

--- app/src/main/java/com/clocle/huxiang/clocle/Bmob_UserBean.java
+++ app/src/main/java/com/clocle/huxiang/clocle/Bmob_UserBean.java
@@ -7,6 +7,61 @@ import cn.bmob.v3.datatype.BmobFile;
 public class Bmob_UserBean extends BmobUser {
    private BmobFile userphoto;
     private String photoUrl;
+    private String sex;
+    private String school;
+    private String birthdate;
+    private String signature;
+    private Integer score;
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public Integer getClocleMoney() {
+        return clocleMoney;
+    }
+
+    public void setClocleMoney(Integer clocleMoney) {
+        this.clocleMoney = clocleMoney;
+    }
+
+    private Integer clocleMoney;
 
     public String getphotoUrl() {
         return photoUrl;
